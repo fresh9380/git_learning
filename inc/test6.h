7b4e7fbd6d3d91caf9085f9a399fa54df9db6b5b
fresh9380 <619645073@qq.com>
2022-06-27 22:20:17 +0800

    first_try

---
+++ inc/test6.h
@@ -0,0 +1,7 @@
+#ifndef _TEST6_H_
+#define _TEST6_H_
+
+
+
+
+#endif
---
+++ src/test6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// #include "test6.h"
+
+
+int main()
+{
+    int num1,num2,sum;
+    printf("12377777777714155557\r\n");
+    printf("input two number:\r\n");
+    scanf("%d,%d",&num1,&num2);
+    sum = num1 + num2 ;
+    printf("the sum is:%d\r\n",sum);
+    printf("hello world!224\r\n");
+    return 0;
+}
